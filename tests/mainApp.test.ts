82aa33b591d19246b2f0e5e4127e2831808fe282
Matias Raimondi <matiraimondi1@gmail.com>
2024-10-31 18:45:59 -0300

    Cambios en el test de MainApp
    + beforeEach

--- tests/mainApp.test.ts
+++ tests/mainApp.test.ts
@@ -3,30 +3,33 @@ import ListaTareas from "../src/clases/listaTareas";
 import MainApp from "../src/clases/mainApp";
 
 describe('Tests de la clase mainApp', () => {    
+
+    let mainApp: MainApp;
+    let expDate: Date;
+    let tarea: Tarea;
+
+    beforeEach(() => {
+        mainApp = new MainApp();
+        expDate = new Date(2024, 10, 31);
+        tarea = new Tarea("Prueba", "Descripcion", expDate, "Media", "Categoria", "Tag")
+    })
+
     test('Añadir tarea a la lista de tareas.', () => {
-        const mainApp = new MainApp();
-        const expDate = new Date(2024, 10, 31);
-        const tarea = new Tarea("Prueba", "Descripcion", expDate, "Media", "Categoria", "Tag")
         mainApp.addTask(tarea);
         const listaTareas = mainApp.getTareasIncompletas()
-        expect(listaTareas.pop()).toBeInstanceOf(Tarea);
+        expect(listaTareas.pop()).toBe(tarea);
     });
 
     test('Remover tarea de la lista de tareas', () => {
-        const mainApp = new MainApp();
-        const expDate = new Date(2024, 10, 31);
-        const tarea = new Tarea("Prueba", "Descripcion", expDate, "Media", "Categoria", "Tag")
         mainApp.addTask(tarea);
-        expect(mainApp.removeTask(tarea)).toBeInstanceOf(Tarea);
+        expect(mainApp.removeTask(tarea)).toBe(tarea);
     });
 
     test('Obtener lista de tareas completas', () => {
-        const mainApp = new MainApp();
         expect(mainApp.getTareasCompletas()).toBeInstanceOf(ListaTareas);
     });
 
     test('Obtener lista de tareas incompletas', () => {
-        const mainApp = new MainApp();
         expect(mainApp.getTareasIncompletas()).toBeInstanceOf(ListaTareas);
     })
 });
