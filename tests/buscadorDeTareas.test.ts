51eec58f7812ebe885d72ffcea215cd7455daed0
Santino <agatiello.santino@gmail.com>
2024-11-04 16:48:29 -0300

    Cambios en el test de BuscadorDeTareas
    Debido a los nuevos cambios en el Enum de Prioridad

--- tests/buscadorDeTareas.test.ts
+++ tests/buscadorDeTareas.test.ts
@@ -2,6 +2,7 @@ import {mock} from "jest-mock-extended";
 import ListaTareas from "../src/clases/listaTareas"
 import BuscadorDeTareas from "../src/clases/buscadorDeTareas"
 import Tarea from "../src/clases/tarea"
+import { PRIORIDAD } from "../src/enums/prioridad";
 
 describe("Prueba del buscador de tareas", () =>{
 
@@ -14,9 +15,9 @@ describe("Prueba del buscador de tareas", () =>{
     beforeEach(() =>{
         expdate = new Date(2024, 11, 5)
         lista= new ListaTareas()
-        tarea1 = new Tarea("Mi Titulo", "Mi Descripcion", expdate , "ALTA", "micategoria", "mietiqueta")
-        tarea2 = new Tarea("El Titulo", "Mi Descripcion", expdate , "BAJA", "micategoria", "mietiqueta")
-        tarea3 = new Tarea("Sin Titulo", "Mi Descripcion", new Date("2019-01-18"), "MEDIA", "micategoria", "mietiqueta")
+        tarea1 = new Tarea("Mi Titulo", "Mi Descripcion", expdate , PRIORIDAD.ALTA, "micategoria", "mietiqueta")
+        tarea2 = new Tarea("El Titulo", "Mi Descripcion", expdate , PRIORIDAD.BAJA, "micategoria", "mietiqueta")
+        tarea3 = new Tarea("Sin Titulo", "Mi Descripcion", new Date("2019-01-18"), PRIORIDAD.MEDIA, "micategoria", "mietiqueta")
         lista.push(tarea1);
         lista.push(tarea2);
         lista.push(tarea3);
