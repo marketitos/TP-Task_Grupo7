6b27e166eeeba66cbc2e048f6dcdb445cdb2cbec
Santino <agatiello.santino@gmail.com>
2024-11-05 22:46:29 -0300

    Eliminacion de metodos en ListaTareas
    se eliminaron los metodos de sort, insertOrdered, e insertUnique. Los primeros dos son eliminados debido a que se implementan de manera mas efectiva en OrdenadorDeTareas, el ultimo es eliminado debido a que utiliza el metodo de insertOrdered. En caso de ser necesario, lo podriamos volver a implementar de manera distinta.

--- src/clases/listaTareas.ts
+++ src/clases/listaTareas.ts
@@ -91,41 +91,8 @@ export class ListaTareas {
         return value;
     }
 
-    /**
-     * Permite insertar y devolver un nodo conservando el orden.
-     * @param value valor del nodo a insertar.
-     * @returns 
-     */
-    public insertOrdered(value: Tarea): NodeTarea {
-        const node = new NodeTarea(value);
-        let headAux = this.head;
-        let previous:NodeTarea = undefined as unknown as NodeTarea;
-        while(headAux && headAux.value < value) {
-            previous = headAux;
-            headAux = headAux.next;
-        }
-        if (!previous) {
-            this.head = node;
-        } else {
-            previous.next = node;
-        }
-        node.next = headAux;
-        return node;
-    }
 
-    /**
-     * Permite insertar un nodo respetando el orden de la lista siempre y cuando el mismo no exista previamente en la lista. Devuelve el puntero al nodo encontrado o insertado.
-     * @param value valor del nodo a insertar.
-     * @returns 
-     */
-    public insertUnique(value: Tarea): NodeTarea {
-        const node = this.search(value);
-        if (!node) {
-            this.insertOrdered(value);
-        }
-        return node;
-    }
-
+  
     /**
      * Permite eliminar el nodo que contiene el valor pasado por parametro.
      * @param value valor del nodo a eliminar.
@@ -157,20 +124,6 @@ export class ListaTareas {
             headAux = headAux.next;
         }
         return headAux;
-    }
-    
-    /**
-     * Permite ordenar una lista.
-     */
-    public sort(): void {
-        let value: Tarea;
-        let listAux = new ListaTareas();
-        while(this.head) {
-            value = this.removeFirst();
-            listAux.insertOrdered(value);
-        }
-        this.head = listAux.head;
-        listAux.clear();
     }
 
     /**
