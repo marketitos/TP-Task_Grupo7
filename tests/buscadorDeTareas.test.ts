ad2576fed6df2f608e4e6ac072c43f2cd3dcb835
Matias Raimondi <matiraimondi1@gmail.com>
2024-11-17 19:51:44 -0300

    Cambios en los tests de buscadores existentes y agregados nuevos buscadores.

--- tests/buscadorDeTareas.test.ts
+++ tests/buscadorDeTareas.test.ts
@@ -1,68 +1,83 @@
-import {mock} from "jest-mock-extended";
-import {ListaTareas} from "../src/clases/listaTareas"
-import {BuscadorDeTareas} from "../src/clases/buscadorDeTareas"
-import {Tarea} from "../src/clases/tarea"
+import { mock } from "jest-mock-extended";
+import { ListaTareas } from "../src/clases/listaTareas"
+import { BuscadorDeTareas } from "../src/clases/buscadorDeTareas"
+import { Tarea } from "../src/clases/tarea"
 import { errorTareaNoExiste } from "../src/excepciones/errorTareaNoExiste";
 import { NodeTarea } from "../src/clases/nodeTarea";
 
 
-describe("Tests unitarios para la clase BuscadorDeTareas", () =>{
+describe("Tests unitarios para la clase BuscadorDeTareas", () => {
 
-    let listaTareasMock: ReturnType<typeof mock<ListaTareas>>;
-    let tareaMock1: ReturnType<typeof mock<Tarea>>;
-    let nodeTareaMock1: ReturnType<typeof mock<NodeTarea>>;
-    let tareaMock2: ReturnType<typeof mock<Tarea>>;
-    let nodeTareaMock2: ReturnType<typeof mock<NodeTarea>>;
-    let buscador: BuscadorDeTareas;
+  let listaTareasMock: ReturnType<typeof mock<ListaTareas>>;
+  let tareaMock1: ReturnType<typeof mock<Tarea>>;
+  let nodeTareaMock1: ReturnType<typeof mock<NodeTarea>>;
+  let tareaMock2: ReturnType<typeof mock<Tarea>>;
+  let nodeTareaMock2: ReturnType<typeof mock<NodeTarea>>;
+  let buscador: BuscadorDeTareas;
 
-    beforeEach(() =>{
-        listaTareasMock = mock<ListaTareas>();
-        tareaMock1 = mock<Tarea>();
-        nodeTareaMock1 = mock<NodeTarea>();
-        tareaMock2 = mock<Tarea>();
-        nodeTareaMock2 = mock<NodeTarea>();
+  beforeEach(() => {
+    listaTareasMock = mock<ListaTareas>();
+    tareaMock1 = mock<Tarea>();
+    nodeTareaMock1 = mock<NodeTarea>();
+    tareaMock2 = mock<Tarea>();
+    nodeTareaMock2 = mock<NodeTarea>();
 
-        let valueMock1 = tareaMock1;
-        Object.defineProperty(nodeTareaMock1, "value", {
-          get: () => valueMock1,
-          set: (newValue) => {
-            valueMock1 = newValue;
-          },
-        });
-
-        let valueMock2 = tareaMock2;
-        Object.defineProperty(nodeTareaMock2, "value", {
-          get: () => valueMock2,
-          set: (newValue) => {
-            valueMock2 = newValue;
-          },
-        });
-        
-        listaTareasMock.getHead.mockReturnValue(nodeTareaMock1);
-        buscador = new BuscadorDeTareas;
+    let valueMock1 = tareaMock1;
+    Object.defineProperty(nodeTareaMock1, "value", {
+      get: () => valueMock1,
+      set: (newValue) => {
+        valueMock1 = newValue;
+      },
+    });
+    Object.defineProperty(nodeTareaMock1, "next", {
+      get: () => nodeTareaMock2
     })
 
-    test("Prueba de buscar una tarea por titulo", () => {
-        tareaMock1.getTitle.mockReturnValue("Tarea 1");
-        tareaMock2.getTitle.mockReturnValue("Tarea 2");
-        expect(buscador.searchByTitle(listaTareasMock, "Tarea 1")).toBe(tareaMock1);
-    })
+    let valueMock2 = tareaMock2;
+    Object.defineProperty(nodeTareaMock2, "value", {
+      get: () => valueMock2,
+      set: (newValue) => {
+        valueMock2 = newValue;
+      },
+    });
 
-    test("Prueba de buscar una tarea por fecha de vencimiento", () =>{
-        const fechaMock1 = new Date("2024-11-15");
-        const fechaMock2 = new Date("2024-11-17");
-        tareaMock1.getExpirationDate.mockReturnValue(fechaMock1);
-        tareaMock2.getExpirationDate.mockReturnValue(fechaMock2);
-        expect(buscador.searchByExpirationDate(listaTareasMock, fechaMock1)).toStrictEqual([tareaMock1]);
-    })
+    listaTareasMock.getHead.mockReturnValue(nodeTareaMock1);
+    buscador = new BuscadorDeTareas;
+  })
 
-    test("Prueba de tirar la exepcion", () =>{
-        tareaMock1.getTitle.mockReturnValue("Tarea 1");
-        tareaMock2.getTitle.mockReturnValue("Tarea 2");
-        try {
-            buscador.searchByTitle(listaTareasMock, "Titulo invalido");
-        } catch (error) {
-            expect(error).toBeInstanceOf(errorTareaNoExiste);
-        }
-    })
+  test("Prueba de buscar una tarea por titulo", () => {
+    tareaMock1.getTitle.mockReturnValue("Tarea 1");
+    tareaMock2.getTitle.mockReturnValue("Tarea 2");
+    expect(buscador.searchByTitle(listaTareasMock, "Tarea 1")).toBe(tareaMock1);
+  })
+
+  test("Prueba de buscar una tarea por fecha de vencimiento", () => {
+    const fechaMock1 = new Date("2024-11-15");
+    const fechaMock2 = new Date("2024-11-17");
+    tareaMock1.getExpirationDate.mockReturnValue(fechaMock1);
+    tareaMock2.getExpirationDate.mockReturnValue(fechaMock2);
+    expect(buscador.searchByExpirationDate(listaTareasMock, fechaMock1)).toStrictEqual([tareaMock1]);
+  })
+
+  test("Prueba de buscar una tarea por categoria", () => {
+    tareaMock1.getCategory.mockReturnValue("a");
+    tareaMock2.getCategory.mockReturnValue("b");
+    expect(buscador.searchByCategory(listaTareasMock, "a")).toStrictEqual([tareaMock1]);
+  })
+
+  test("Prueba de buscar una tarea por tag", () => {
+    tareaMock1.getTags.mockReturnValue(["a", "b"]);
+    tareaMock2.getTags.mockReturnValue(["c", "d"]);
+    expect(buscador.searchByTag(listaTareasMock, "c")).toStrictEqual([tareaMock2])
+  })
+
+  test("Prueba de tirar la exepcion", () => {
+    tareaMock1.getTitle.mockReturnValue("Tarea 1");
+    tareaMock2.getTitle.mockReturnValue("Tarea 2");
+    try {
+      buscador.searchByTitle(listaTareasMock, "Titulo invalido");
+    } catch (error) {
+      expect(error).toBeInstanceOf(errorTareaNoExiste);
+    }
+  })
 })
