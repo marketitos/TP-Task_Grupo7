0be1f7ffa7a9653928ac5eb0f5d091c8b83b8869
Matias Raimondi <matiraimondi1@gmail.com>
2024-11-17 21:13:09 -0300

    Documentación de la clase Estadisticas

--- src/clases/estadisticas.ts
+++ src/clases/estadisticas.ts
@@ -1,11 +1,20 @@
 import { errorTareaNoExiste } from "../excepciones/errorTareaNoExiste";
 import { ListaTareas } from "./listaTareas";
 
+/**
+ * Clase que permite obtener las estadisticas con respecto a las tareas pendientes.
+ */
 export class Estadisticas {
     constructor() {
 
     }
 
+    /**
+     * Compara la cantidad de tareas pendientes con la cantidad de tareas completas para devpñver la taza de finalización.
+     * @param tareasPendientes lista de tareas pendientes.
+     * @param tareasCompletas lista de tareas completas.
+     * @returns 
+     */
     public getTasaDeFinalizacion(tareasPendientes: ListaTareas, tareasCompletas: ListaTareas): number {
         let countPendientes = 0;
         let countCompletas = 0;
@@ -33,6 +42,11 @@ export class Estadisticas {
         return (countCompletas / (countCompletas + countPendientes)) * 100;
     }
 
+    /**
+     * Recorre la lista de tareas pendientes y devuelve el promedio de los porcentajes de avance.
+     * @param tareasPendientes lista de tareas pendientes.
+     * @returns 
+     */
     public getPromedioPorcentajes(tareasPendientes: ListaTareas): number {
         let headAux = tareasPendientes.getHead();
         let countTareas = 0;
